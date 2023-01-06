1d627f229b8ae96fee9ae6e87559f6d8489ca5d8
lucsantos21 <luc.olssay@outlook.com>
2023-01-06 15:00:04 -0300

    Feat(UserProfile): Add migration

---
+++ Otzzei.MetchJob/Otzzei.MetchJob.Infrastructure/Migrations/20230106175924_AddUserProfile.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Otzzei.MetchJob.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserProfile : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserProfiles",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserProfiles", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserProfiles");
+        }
+    }
+}
