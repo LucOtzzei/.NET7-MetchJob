662c109bdb663c9b260935deddc1bb1b023d0f61
lucsantos21 <luc.olssay@outlook.com>
2023-01-06 15:50:09 -0300

    Feat(JobOpportunity): add migration

---
+++ Otzzei.MetchJob/Otzzei.MetchJob.Infrastructure/Migrations/20230106184822_AddJobOpportunity.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Otzzei.MetchJob.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddJobOpportunity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "JobOpportunities",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ManagerJobId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    JobTitle = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SalaryCurrency = table.Column<int>(type: "int", nullable: false),
+                    MinimumWageAtWork = table.Column<double>(type: "float", nullable: false),
+                    MaximumWageAtWork = table.Column<double>(type: "float", nullable: false),
+                    JobsDescription = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    JobActivities = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_JobOpportunities", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "JobOpportunities");
+        }
+    }
+}
