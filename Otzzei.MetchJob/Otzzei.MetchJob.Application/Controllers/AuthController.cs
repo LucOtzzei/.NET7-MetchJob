5c9e5b40d4d83a1f2c1e4bdbcc348a030bd4cdcb
lucsantos21 <luc.olssay@outlook.com>
2023-01-10 11:37:18 -0300

    Feat(Auth): AuthController created

---
+++ Otzzei.MetchJob/Otzzei.MetchJob.Application/Controllers/AuthController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Otzzei.MetchJob.Application.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _loginService;
+
+        public AuthController(IAuthService loginService)
+        {
+            _loginService = loginService;
+        }
+        [HttpPost]
+        public async Task<IActionResult> LoginAsync(LoginRequest request)
+        {
+            var response = await _loginService.LoginAsync(request);
+            return Ok(response);
+        }
+        [HttpGet("request-password-recovery")]
+        public async Task<IActionResult> RequestPasswordRecoveryAsync(PasswordRecoveryRequest request)
+        {
+            var response = await _loginService.RequestPasswordRecoveryAsync(request);
+            return Ok(response);
+        }
+        [HttpGet("change-password")]
+        public async Task<IActionResult> RecoverPasswordAsync(ChangePasswordRequest request)
+        {
+            var response = await _loginService.RecoverPasswordAsync(request);
+            return Ok(response);
+        }
+    }
+}
